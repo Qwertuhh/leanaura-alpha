da710ddaeaa24156f29edea521fc963af0a200c1
Qwertuhh <arihantjain132407+github@gmail.com>
2025-08-04 21:16:05 +0530

    styling import fixed

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Layout from "@/Layout";
+import App from "@/App";
+import Notebook from "@/pages/notebook/App";
+import Notebooks from "@/pages/notebooks/App";
 import "@/index.css";
 import "@/styles/markdown/table.css";
 import "@/styles/globals.css";
 import "@/styles/markdown/markdown.css";
 import "@/styles/canvas.css";
-import Layout from "@/Layout";
-import App from "@/App";
-import Notebook from "@/pages/notebook/App";
-import Notebooks from "@/pages/notebooks/App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
